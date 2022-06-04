48c148592c6c5c58e56072cdb518e41492194a6f
Brenda <breeeendaqn@gmail.com>
2022-06-04 02:09:21 -0300

    define app stylesheet

---
+++ helloApp/src/styles/style.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 45,
+      alignSelf: 'center',
+    },
+    title: {
+      fontSize: 40,
+    },
+    counter: {
+      fontSize: 30,
+      padding: 20,
+      alignSelf: 'center',
+    },
+    button: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+  });
